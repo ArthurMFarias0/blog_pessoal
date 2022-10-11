0491818cf5fba47395a536d94d763a05890b85b2
Arthur <arthuremeunome@gmail.com>
2022-10-11 15:56:49 -0300

    Blog Pessoal Back-End

---
+++ src/tema/services/tema.service.ts
@@ -0,0 +1,68 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
+import { Tema } from "../entities/tema.entity";
+
+
+@Injectable()
+export class TemaService {
+    constructor(
+        @InjectRepository(Tema)
+        private temaReporsitory: Repository<Tema>
+    ) {}
+
+    async findAll (): Promise<Tema[]>{
+        return await this.temaReporsitory.find({
+            relations: {
+                postagem: true
+            }
+        })
+    }
+
+    async findById (id: number): Promise<Tema> {
+        let tema = await this.temaReporsitory.findOne({
+            where: {
+                id
+            },
+            relations: {
+                postagem: true
+            }
+        });
+
+        if (!tema)
+            throw new HttpException('Tema não encontrado!',HttpStatus.NOT_FOUND)
+        return tema;
+    }
+
+    async findByDescricao (descricao: string): Promise<Tema[]> {
+        return await this.temaReporsitory.find({
+            where: {
+                descricao: ILike(`%${descricao}%`)
+            },
+            relations: {
+                postagem: true
+            }
+        })
+    }
+
+    async create (tema: Tema): Promise<Tema> {
+        return await this.temaReporsitory.save(tema);
+    }
+
+    async update (tema: Tema): Promise<Tema> {
+        let buscaTema = await this.findById(tema.id)
+
+        if(!buscaTema || !tema.id)
+            throw new HttpException('Tema não encontrado', HttpStatus.NOT_FOUND)
+        return await this.temaReporsitory.save(tema)
+    }
+
+    async delete (id: number): Promise<DeleteResult>{
+        let buscaTema = await this.findById(id)
+
+        if(!buscaTema)
+            throw new HttpException('Tema não encontrado', HttpStatus.NOT_FOUND)
+            
+        return await this.temaReporsitory.delete(id)
+    }
+}
